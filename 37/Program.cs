8357c5e6528e54b9ad6e513ba89feb8b14aecfc1
Анастасия <understooooood@mail.ru>
2022-11-02 21:18:33 +0300

    решили 37 задачу

---
+++ 37/Program.cs
@@ -0,0 +1,47 @@
+﻿// Найдите произведение пар чисел в одномерном массиве.
+// Парой считаем первый и последний элемент, второй и предпоследний
+// и т.д. Результат запишите в новом массиве.
+// [1 2 3 4 5] -> 5 8 3
+// [6 7 3 6] -> 36 21
+
+
+
+int[] GetArray(string stringArray)  // функция которая делает с клавиатуры массив
+{
+    string[] nums = stringArray.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    int[] result = new int[nums.Length];
+
+    for (int i = 0; i < nums.Length; i++)
+    {
+        result[i] = int.Parse(nums[i]);
+    }
+    return result;
+}
+
+int[] ResultArray(int[] array)
+{
+    int size = array.Length / 2;
+    if (array.Length % 2 !=0)
+    {
+        size++;
+    }
+
+    int[] result = new int[size];
+    for (int i = 0; i < array.Length / 2; i++)
+    {
+        result [i] = array[i]*array[array.Length-1-i];
+        if (array.Length % 2 !=0)
+        {
+            result[size-1] = array[array.Length/2];
+        }
+    }
+    return result;
+}
+
+Console.Clear();
+Console.WriteLine("Введите массив через пробел:");
+string input = Console.ReadLine();
+int[] numArray = GetArray(input);
+
+int[] newArray = ResultArray (numArray);
+Console.WriteLine(String.Join(", ", newArray));
